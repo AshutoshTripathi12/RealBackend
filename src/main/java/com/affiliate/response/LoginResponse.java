3daab69ef780dbe9fecd3c77fa3e038ae88c71d5
ashutosh <ashutosh@example.com>
2025-05-24 22:26:35 +0530

    'commit'

--- src/main/java/com/affiliate/response/LoginResponse.java
+++ src/main/java/com/affiliate/response/LoginResponse.java
@@ -4,8 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class LoginResponse {
 	private String token;
 
+	public LoginResponse(String token)
+	{
+		this.token=token;
+	}
 }
